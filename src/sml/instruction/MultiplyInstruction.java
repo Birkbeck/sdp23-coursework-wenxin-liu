158c583f87e2de2fafba32dbea53c8b3b15947f9
Wenxin Liu <wenxin.liu@springernature.com>
2023-02-24 16:02:43 +0000

    Implement multiply instruction

---
+++ src/sml/instruction/MultiplyInstruction.java
@@ -0,0 +1,30 @@
+package sml.instruction;
+
+import sml.Instruction;
+import sml.Machine;
+import sml.RegisterName;
+
+public class MultiplyInstruction extends Instruction {
+    private final RegisterName result;
+    private final RegisterName source;
+    public static final String OP_CODE = "mul";
+
+    public MultiplyInstruction(String label, RegisterName result, RegisterName source) {
+        super(label, OP_CODE);
+        this.result = result;
+        this.source = source;
+    }
+
+    @Override
+    public int execute(Machine m) {
+        int value1 = m.getRegisters().get(result);
+        int value2 = m.getRegisters().get(source);
+        m.getRegisters().set(result, value1 * value2);
+        return NORMAL_PROGRAM_COUNTER_UPDATE;
+    }
+
+    @Override
+    public String toString() {
+        return null;
+    }
+}
---
+++ test/sml/instruction/MultiplyInstructionTest.java
@@ -0,0 +1,39 @@
+package sml.instruction;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import sml.Instruction;
+import sml.Machine;
+import sml.Registers;
+
+import static sml.Registers.Register.EAX;
+import static sml.Registers.Register.EBX;
+
+public class MultiplyInstructionTest {
+    private Machine machine;
+    private Registers registers;
+
+    @BeforeEach
+    void setUp() {
+        machine = new Machine(new Registers());
+        registers = machine.getRegisters();
+        //...
+    }
+
+    @AfterEach
+    void tearDown() {
+        machine = null;
+        registers = null;
+    }
+
+    @Test
+    void executeValid() {
+        registers.set(EAX, 5);
+        registers.set(EBX, 6);
+        Instruction instruction = new MultiplyInstruction(null, EAX, EBX);
+        instruction.execute(machine);
+        Assertions.assertEquals(30, machine.getRegisters().get(EAX));
+    }
+}
