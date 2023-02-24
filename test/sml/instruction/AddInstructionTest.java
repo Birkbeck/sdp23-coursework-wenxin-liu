e680cfcb5eaeec11bef6b27b41a761be0b04668c
Wenxin Liu <wenxin.liu@springernature.com>
2023-02-24 15:02:02 +0000

    Import JUnit 5.8.1, also format code

--- test/sml/instruction/AddInstructionTest.java
+++ test/sml/instruction/AddInstructionTest.java
@@ -12,37 +12,37 @@ import sml.Registers;
 import static sml.Registers.Register.*;
 
 class AddInstructionTest {
-  private Machine machine;
-  private Registers registers;
+    private Machine machine;
+    private Registers registers;
 
-  @BeforeEach
-  void setUp() {
-    machine = new Machine(new Registers());
-    registers = machine.getRegisters();
-    //...
-  }
+    @BeforeEach
+    void setUp() {
+        machine = new Machine(new Registers());
+        registers = machine.getRegisters();
+        //...
+    }
 
-  @AfterEach
-  void tearDown() {
-    machine = null;
-    registers = null;
-  }
+    @AfterEach
+    void tearDown() {
+        machine = null;
+        registers = null;
+    }
 
-  @Test
-  void executeValid() {
-    registers.set(EAX, 5);
-    registers.set(EBX, 6);
-    Instruction instruction = new AddInstruction(null, EAX, EBX);
-    instruction.execute(machine);
-    Assertions.assertEquals(11, machine.getRegisters().get(EAX));
-  }
+    @Test
+    void executeValid() {
+        registers.set(EAX, 5);
+        registers.set(EBX, 6);
+        Instruction instruction = new AddInstruction(null, EAX, EBX);
+        instruction.execute(machine);
+        Assertions.assertEquals(11, machine.getRegisters().get(EAX));
+    }
 
-  @Test
-  void executeValidTwo() {
-    registers.set(EAX, -5);
-    registers.set(EBX, 6);
-    Instruction instruction = new AddInstruction(null, EAX, EBX);
-    instruction.execute(machine);
-    Assertions.assertEquals(1, machine.getRegisters().get(EAX));
-  }
+    @Test
+    void executeValidTwo() {
+        registers.set(EAX, -5);
+        registers.set(EBX, 6);
+        Instruction instruction = new AddInstruction(null, EAX, EBX);
+        instruction.execute(machine);
+        Assertions.assertEquals(1, machine.getRegisters().get(EAX));
+    }
 }
