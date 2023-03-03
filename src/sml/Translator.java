3a7e8903c7a4a525cc9da0be2af8006aa4462280
Wenxin Liu <wenxin.liu@springernature.com>
2023-03-03 20:00:29 +0000

    Fix bug in reflection implementation of Translator.getInstruction()

--- src/sml/Translator.java
+++ src/sml/Translator.java
@@ -68,102 +68,102 @@ public final class Translator {
             return null;
 
         String opcode = scan();
-        switch (opcode) {
-            case AddInstruction.OP_CODE -> {
-                String r = scan();
-                String s = scan();
-                return new AddInstruction(label, Register.valueOf(r), Register.valueOf(s));
-            }
-
-            // add code for all other types of instructions
-            case MulInstruction.OP_CODE -> {
-                String resultRegister = scan();
-                String sourceRegister = scan();
-                return new MulInstruction(label, Register.valueOf(resultRegister), Register.valueOf(sourceRegister));
-            }
-
-            case SubInstruction.OP_CODE -> {
-                String resultRegister = scan();
-                String sourceRegister = scan();
-                return new SubInstruction(label, Register.valueOf(resultRegister), Register.valueOf(sourceRegister));
-            }
-
-            case DivInstruction.OP_CODE -> {
-                String resultRegister = scan();
-                String sourceRegister = scan();
-                return new DivInstruction(label, Register.valueOf(resultRegister), Register.valueOf(sourceRegister));
-            }
-
-            case OutInstruction.OP_CODE -> {
-                String targetRegister = scan();
-                return new OutInstruction(label, Register.valueOf(targetRegister));
-            }
-
-            case MovInstruction.OP_CODE -> {
-                String targetRegister = scan();
-                String sourceRegister = scan();
-                return new MovInstruction(label, Register.valueOf(targetRegister), Integer.valueOf(sourceRegister));
-            }
-
-            case JnzInstruction.OP_CODE -> {
-                String register = scan();
-                String jumpToLabel = scan();
-                return new JnzInstruction(label, Register.valueOf(register), jumpToLabel);
-            }
-            default -> {
-                System.out.println("Unknown instruction: " + opcode);
-            }
-        }
-
-        return null;
-//
-//        // Then, replace the switch by using the Reflection API
-//        try {
-//
-//            Class<?> classObject = Class.forName(
-//                    String.format("sml.instruction.%sInstruction",
-//                            opcode.substring(0, 1).toUpperCase() + opcode.substring(1)
-//                    )
-//            );
-//
-//            Constructor<?> constructor = classObject.getConstructors()[0];
-//
-//            ArrayList<String> argumentsList = new ArrayList<>();
-//            argumentsList.add(label);
-//
-//            for (int i = 1; i < constructor.getParameterCount(); i++) {
-//                argumentsList.add(scan());
+//        switch (opcode) {
+//            case AddInstruction.OP_CODE -> {
+//                String r = scan();
+//                String s = scan();
+//                return new AddInstruction(label, Register.valueOf(r), Register.valueOf(s));
 //            }
 //
-//            Object[] parameterObjects = new Object[constructor.getParameterCount()];
-//
-//            Class<?>[] constructorParameterTypes = constructor.getParameterTypes();
-//
-//            for (int i = 0; i < constructor.getParameterCount(); i++) {
-//                Class<?> constructionParameterTypeClass = constructorParameterTypes[i];
-//
-//                if (i == 0 && argumentsList.get(i) == null) {
-//                    parameterObjects[i] = null;
-//
-//                } else if (constructionParameterTypeClass.isInterface()) {
-//                    Method method = Registers.Register.class.getMethod("valueOf", String.class);
-//
-//                    parameterObjects[i] = method.invoke(null, argumentsList.get(i));
-//
-//                } else {
-//                    parameterObjects[i] = constructionParameterTypeClass
-//                            .getConstructor(String.class)
-//                            .newInstance(argumentsList.get(i));
-//                }
+//            // add code for all other types of instructions
+//            case MulInstruction.OP_CODE -> {
+//                String resultRegister = scan();
+//                String sourceRegister = scan();
+//                return new MulInstruction(label, Register.valueOf(resultRegister), Register.valueOf(sourceRegister));
 //            }
 //
-//            return (Instruction) constructor.newInstance(parameterObjects);
+//            case SubInstruction.OP_CODE -> {
+//                String resultRegister = scan();
+//                String sourceRegister = scan();
+//                return new SubInstruction(label, Register.valueOf(resultRegister), Register.valueOf(sourceRegister));
+//            }
 //
-//        } catch (Exception e) {
-//            e.printStackTrace();
+//            case DivInstruction.OP_CODE -> {
+//                String resultRegister = scan();
+//                String sourceRegister = scan();
+//                return new DivInstruction(label, Register.valueOf(resultRegister), Register.valueOf(sourceRegister));
+//            }
+//
+//            case OutInstruction.OP_CODE -> {
+//                String targetRegister = scan();
+//                return new OutInstruction(label, Register.valueOf(targetRegister));
+//            }
+//
+//            case MovInstruction.OP_CODE -> {
+//                String targetRegister = scan();
+//                String sourceRegister = scan();
+//                return new MovInstruction(label, Register.valueOf(targetRegister), Integer.valueOf(sourceRegister));
+//            }
+//
+//            case JnzInstruction.OP_CODE -> {
+//                String register = scan();
+//                String jumpToLabel = scan();
+//                return new JnzInstruction(label, Register.valueOf(register), jumpToLabel);
+//            }
+//            default -> {
+//                System.out.println("Unknown instruction: " + opcode);
+//            }
 //        }
 //
 //        return null;
+
+        // Then, replace the switch by using the Reflection API
+        try {
+
+            Class<?> classObject = Class.forName(
+                    String.format("sml.instruction.%sInstruction",
+                            opcode.substring(0, 1).toUpperCase() + opcode.substring(1)
+                    )
+            );
+
+            Constructor<?> constructor = classObject.getConstructors()[0];
+
+            ArrayList<String> argumentsList = new ArrayList<>();
+            argumentsList.add(label);
+
+            for (int i = 1; i < constructor.getParameterCount(); i++) {
+                argumentsList.add(scan());
+            }
+
+            Object[] parameterObjects = new Object[constructor.getParameterCount()];
+
+            Class<?>[] constructorParameterTypes = constructor.getParameterTypes();
+
+            for (int i = 0; i < constructor.getParameterCount(); i++) {
+                Class<?> constructionParameterTypeClass = constructorParameterTypes[i];
+
+                if (i == 0 && argumentsList.get(i) == null) {
+                    parameterObjects[i] = null;
+
+                } else if (constructionParameterTypeClass.isInterface()) {
+                    Method method = Registers.Register.class.getMethod("valueOf", String.class);
+
+                    parameterObjects[i] = method.invoke(null, argumentsList.get(i));
+
+                } else {
+                    parameterObjects[i] = constructionParameterTypeClass
+                            .getConstructor(String.class)
+                            .newInstance(argumentsList.get(i));
+                }
+            }
+
+            return (Instruction) constructor.newInstance(parameterObjects);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
 //
 //        // Next, use dependency injection to allow this machine class
 //        // to work with different sets of opcodes (different CPUs)
